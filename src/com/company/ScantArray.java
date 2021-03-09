905341a21a404c101443929f101fd04f18c3845d
hslaney <haley.slaney@gmail.com>
2021-03-09 13:19:48 -0500

    not initial

--- src/com/company/ScantArray.java
+++ src/com/company/ScantArray.java
@@ -164,3 +164,36 @@ public class ScantArray {
     }
 }
 
+/*
+1
+-9
+0
+rows 4
+columns 5
+0 0 0 0 0
+0 5 0 0 4
+1 0 0 0 0
+0 -9 0 0 0
+
+0 0 0 0
+0 0 0 4
+1 0 0 0
+0 0 0 0
+
+rows 3
+columns 6
+value at (2,2) is 0
+0 0 0 -8 0 1
+0 7 0 0 0 4
+3 0 0 5 0 0
+
+
+0 0 -8 1
+0 7 0 4
+3 0 5 0
+
+rows 3
+columns 4
+value at (2,2) is 5
+
+ */
